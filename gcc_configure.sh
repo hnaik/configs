66147a0e3ef30404e9f1fbeb87eafb2db95fc380
Harish Naik <harishgnaik@gmail.com>
2022-04-19 22:22:22 -0500

    Added gcc configure script

---
+++ gcc_configure.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+./configure -v \
+	--prefix=/usr/local/opt/gcc/11.2.0 \
+	--with-pkgversion='Ubuntu 11.2.0-1ubuntu1~20.04.1' \
+	--enable-languages=c,c++ \
+	--prefix=/usr/local/opt/gcc/11.2.0 \
+	--with-gcc-major-version-only \
+	--program-suffix=-11 \
+	--program-prefix=x86_64-linux-gnu- \
+	--enable-shared \
+	--enable-linker-build-id \
+	--libexecdir=/usr/lib \
+	--without-included-gettext \
+	--enable-threads=posix \
+	--libdir=/usr/lib \
+	--enable-nls \
+	--enable-clocale=gnu \
+	--enable-libstdcxx-debug \
+	--enable-libstdcxx-time=yes \
+	--with-default-libstdcxx-abi=new \
+	--enable-gnu-unique-object \
+	--disable-vtable-verify \
+	--enable-plugin \
+	--enable-default-pie \
+	--with-system-zlib \
+	--with-target-system-zlib=auto \
+	--enable-objc-gc=auto \
+	--enable-multiarch \
+	--disable-werror \
+	--with-arch-32=i686 \
+	--with-abi=m64 \
+	--with-multilib-list=m64 \
+	--enable-multilib \
+	--with-tune=generic \
+	--without-cuda-driver \
+	--enable-checking=release \
+	--build=x86_64-linux-gnu \
+	--host=x86_64-linux-gnu \
+	--target=x86_64-linux-gnu
